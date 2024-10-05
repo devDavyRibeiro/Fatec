b3d769ad504e8f37039456fca8292d618dab0651
Davy Ribeiro <davy.oliv.ribeiro@gmail>
2024-10-04 22:09:09 -0300

    Exercício de Estrutura de Dados

---
+++ EstruturaDados/Ex1ES/src/main/java/com/mycompany/ex1es/Ex1ES.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex1es;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ *
+ * @author Alunos
+ */
+public class Ex1ES {
+
+    public static void main(String[] args) {
+        List cor = new ArrayList();
+        
+        cor.add("Vermelho");
+        cor.add("Verde");
+        cor.add("Azul");
+        cor.add("Amarelo");
+        
+        System.out.println(cor);
+        cor.remove(1);
+        
+        Collections.sort(cor);
+        
+        System.out.println(cor);
+        
+        
+    }
+}
