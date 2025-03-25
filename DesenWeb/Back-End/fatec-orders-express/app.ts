3f5c99afd9a6801fcd25b480065b8d0506862b7c
devDavyRibeiro <you@example.comdavy.oliv.ribeiro@gmail.com>
2025-03-25 01:05:03 -0300

    atividade01 de desenvolivmento web3

--- DesenWeb/Back-End/fatec-orders-express/app.ts
+++ DesenWeb/Back-End/fatec-orders-express/app.ts
@@ -31,8 +31,37 @@ const products=  [
         weight: 5,
         meassureUnit:'Kg'
     }
-
+    
 ]
+const clients = [
+    {
+        id:1,
+        name: "João Silva",
+        document: '47251787806',
+        zipCode: '18050600',
+        phone: '15991984343',
+        email: 'joão@gmail.com'
+    },
+    
+    {
+        id:2,
+        name: "Maria Silva",
+        document: '47251785402',
+        zipCode: '18050600',
+        phone: '15991983434',
+        email: '@gmail.com'
+    }
+]
+
+// Define um método post que responde ao path /
+app.get("/", (req : Request, res : Response)=>{
+    res.send("Hello Word Again!");
+});
+// Define um método get que responde ao path /products/
+app.get('/products',(req : Request, res : Response) =>{
+    //res.status --> Código de Resposta HTTP 
+    res.status(200).json(products);
+})
 // Define um método get que responde ao path /products/:id
 app.get('/products/:id', (req : Request, res : Response) =>{
     const product = products.find((product) =>{
@@ -44,16 +73,28 @@ app.get('/products/:id', (req : Request, res : Response) =>{
     }
     res.status(200).json(product)
 })
-// Define um método get que responde ao path /products/
-app.get('/products',(req : Request, res : Response) =>{
-    //res.status --> Código de Resposta HTTP 
-    res.status(200).json(products);
-})
+//cadastro
 app.post("/products", (req : Request, res : Response)=>{
     const product = req.body;
     products.push(product);
     res.status(201).send();
 });
+//atualização Produto
+app.put('/products/:id',(req : Request, res:Response) =>{
+    const productUpdate = req.body;
+
+    const product = products.find((product) =>{
+        return product.id === Number(req.params.id);
+    })
+    if(!product){
+        res.status(404).send();
+        return;
+    }
+
+    products.splice(products.indexOf(product), 1, productUpdate);
+    res.status(200).json(productUpdate);
+})
+//Exclusão Produto
 app.delete('/products/:id',(req : Request, res : Response) =>{
     const product = products.find((product) =>{
         return product.id === Number(req.params.id);
@@ -66,10 +107,58 @@ app.delete('/products/:id',(req : Request, res : Response) =>{
     products.splice(index);
     res.status(200).send();
 })
-// Define um método post que responde ao path /
-app.post("/", (req : Request, res : Response)=>{
-    res.send("Hello Word Again!");
+
+//CLIENT
+
+app.get('/clients',(req : Request, res : Response) =>{
+    //res.status --> Código de Resposta HTTP 
+    res.status(200).json(clients);
+})
+// Define um método get que responde ao path /products/:id
+app.get('/clients/:id', (req : Request, res : Response) =>{
+    const client = clients.find((client) =>{
+        return client.id === Number(req.params.id);
+    })
+    if(!client){
+        res.status(404).send();
+        return;
+    }
+    res.status(200).json(client)
+})
+//cadastro
+app.post("/clients", (req : Request, res : Response)=>{
+    const client = req.body;
+    clients.push(client);
+    res.status(201).json(client);
 });
+//atualização Produto
+app.put('/clients/:id',(req : Request, res:Response) =>{
+    const clientUpdate = req.body;
+
+    const client = clients.find((client) =>{
+        return client.id === Number(req.params.id);
+    })
+    if(!client){
+        res.status(404).send();
+        return;
+    }
+
+    clients.splice(clients.indexOf(client), 1, clientUpdate);
+    res.status(200).json(clientUpdate);
+})
+//Exclusão Produto
+app.delete('/clients/:id',(req : Request, res : Response) =>{
+    const client = clients.find((client) =>{
+        return client.id === Number(req.params.id);
+    })
+    if(!client){
+        res.status(404).send();
+        return;
+    }
+    const index = clients.indexOf(client);
+    clients.splice(index);
+    res.status(200).send();
+})
 
 // Inicia a aplicação na porta 3000 
 app.listen(3000, () => {
